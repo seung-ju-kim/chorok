869ab15427e83f09cd194f1b8d684f6ebbe03b76
brilence75 <brilence75@gmail.com>
2022-05-31 20:37:32 +0900

    back 셋팅 완료

---
+++ back/src/app.js
@@ -0,0 +1,25 @@
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+// CORS 에러 방지
+app.use(cors());
+
+// express 기본 제공 middleware
+// express.json(): POST 등의 요청과 함께 오는 json형태의 데이터를 인식하고 핸들링할 수 있게 함.
+// express.urlencoded: 주로 Form submit 에 의해 만들어지는 URL-Encoded 형태의 데이터를 인식하고 핸들링할 수 있게 함.
+app.use(express.json({
+    limit: "5mb"
+}));
+  app.use(express.urlencoded({ 
+    limit: "5mb",
+    extended: false 
+}));
+
+// 기본 페이지
+app.get("/", (req, res) => {
+    res.send("안녕하세요, AI프로젝트 3팀 서버입니다.");
+});
+
+export { app };
---
+++ back/src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const MONGODB_URI = 
+process.env.MONGODB_URI || "MONGODB_URI가 설정되지 않았습니다..";
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("정상적으로 MongoDB 서버에 연결되었습니다.");
+  })
+  .catch((error) => {
+    console.error("MongoDB 연결에 실패하였습니다..." + "\n" + error);
+  });
