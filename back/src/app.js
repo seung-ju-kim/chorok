571608ba21972d8dd27d109dda4b82ed4d7204a1
kimdahyeon <anniy8920@naver.com>
2022-06-02 13:35:53 +0900

    feat : swagger연동

--- back/src/app.js
+++ back/src/app.js
@@ -1,11 +1,17 @@
 import cors from "cors";
 import express from "express";
-
+import swaggerUi from "swagger-ui-express";
+import { specs } from "./config/swaggerDoc.js";
 const app = express();
 
 // CORS 에러 방지
 app.use(cors());
-
+app.use(
+    "/swagger",
+    swaggerUi.serve,
+    swaggerUi.setup(specs, { explorer: true })
+  );
+  
 // express 기본 제공 middleware
 // express.json(): POST 등의 요청과 함께 오는 json형태의 데이터를 인식하고 핸들링할 수 있게 함.
 // express.urlencoded: 주로 Form submit 에 의해 만들어지는 URL-Encoded 형태의 데이터를 인식하고 핸들링할 수 있게 함.
---
+++ back/src/config/swaggerDoc.js
@@ -0,0 +1,30 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import path from "path";
+const __dirname = path.resolve();
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "version 3.0 team API Docs",
+      version: "1.0.0",
+      description: "Elice 4th 3team ",
+    },
+    components: {
+      securitySchemes: {
+        Authorization: {
+          type: "http",
+          scheme: "Bearer",
+          name: "Authorization",
+          bearerFormat: "JWT",
+          in: "header",
+        },
+      },
+    },
+  },
+  swagger: "2.0",
+  basePath: "localhost:5005/",
+  apis: [__dirname + "/src/routers/*.js", __dirname + "/src/swagger/*"],
+};
+
+export const specs = swaggerJSDoc(options);
