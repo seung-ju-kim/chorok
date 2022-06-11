fec76aeec890a6b5e1effc7adc73ac62bbf7e01b
sj_kim <ksj961223@gmail.com>
2022-06-12 02:29:34 +0900

    [Test] git 연결 테스트

--- front/src/pages/MyGarden.js
+++ front/src/pages/MyGarden.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box } from "@mui/material";
 const MyGarden = () => {
-  return <Box sx={{ mt: 12 }}>MyGarden</Box>;
+  return <Box sx={{ mt: 12 }}>test</Box>;
 };
 
 export default MyGarden;
