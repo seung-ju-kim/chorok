46522c35fc53f49bbf883b6b9dcc2662f04ba05d
kimdahyeon <anniy8920@naver.com>
2022-06-12 11:38:31 +0900

    fix : 서버두개열림 해결

--- data/app.py
+++ data/app.py
@@ -6,4 +6,4 @@ app.config['JSON_AS_ASCII'] = False
 with app.app_context():
     app.register_blueprint(ml)
 if __name__ == '__main__':
-    app.run( port='8000', debug=True)
+    app.run( port='8000', debug=True) #host를 쓰면 local 서버열림
