69dc7ea25109889f8510cb555a2513b74439346a
Kim <rlarhkdwo98@gmail.com>
2022-06-15 00:28:04 +0900

    Feat: 게시판 메인 추가

--- front/src/pages/Community.js
+++ front/src/pages/Community.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { Box } from "@mui/material";
 const Community = () => {
-  return <Box sx={{ mt: 12 }}>Community</Box>;
+  return (
+    <Box sx={{ mt: 12 }}>
+      Community
+    </Box>
+  );
 };
 
 export default Community;
