c50b960183a15edf57b32cf929d9ec9051dac234
brilence75 <brilence75@gmail.com>
2022-06-14 14:40:47 +0900

    Update : comment 모델 등록

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { User } from "./models/User";
 import { Post } from "./models/Post";
+import { Comment } from "./models/Comment";
 
 const DB_URL =
   process.env.MONGODB_URL ||
@@ -16,5 +17,5 @@ db.on("error", (error) =>
   console.error("MongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error)
 );
 
-export { User, Post };
+export { User, Post, Comment };
 
