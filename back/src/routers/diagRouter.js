12c03f747dbddb6c5534df48cacdc669bc33591a
BeenKimKr <bin9610@gmail.com>
2022-06-16 00:20:41 +0900

    Update : diag. router

--- back/src/routers/diagRouter.js
+++ back/src/routers/diagRouter.js
@@ -12,9 +12,10 @@ diagRouter.post("/",
       const { key } = req.file;
       const fileName = String(key).split("diag_img/")[1];
 
-      const diseaseName = await axios.post(`http://localhost:8000/test/${fileName}`);
+      const diseaseName = await axios.get(`http://localhost:8000/predict/${fileName}`);
       const disease = await diagService.getDisease(diseaseName);
-      res.status(200).json(disease);
+      const result = { ...disease, url: key };
+      res.status(200).json(result);
     } catch (error) {
       next(error);
     }
