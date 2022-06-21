8facee5f83389aeb463fede72751d59beb7b7e99
brilence75 <brilence75@gmail.com>
2022-06-21 19:20:49 +0900

    Update : upload, diary 라우터 등록

--- back/src/app.js
+++ back/src/app.js
@@ -4,9 +4,11 @@ import swaggerUi from "swagger-ui-express";
 
 import { specs } from "./config/swaggerDoc.js";
 import { userAuthRouter } from "./routers/userRouter";
+import { uploadRouter } from "./routers/uploadRouter";
 import { postRouter } from "./routers/postRouter";
 import { commentRouter } from "./routers/commentRouter";
 import { plantRouter } from "./routers/plantRouter";
+import { diaryRouter } from "./routers/diaryRouter";
 import { diagRouter } from "./routers/diagRouter";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 
@@ -42,9 +44,11 @@ app.get("/", (req, res) => {
 
 // router, service 구현 (userAuthRouter는 맨 위에 있어야 함.)
 app.use("/users", userAuthRouter);
+app.use(uploadRouter);
 app.use(postRouter);
 app.use(commentRouter);
 app.use(plantRouter);
+app.use(diaryRouter);
 app.use("/diags", diagRouter);
 
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
