ebc7d6c7d3e272b11cac37c04a894e8261f6cd7f
brilence75 <brilence75@gmail.com>
2022-06-21 18:26:22 +0900

    Update: diaryRouter RUD 추가

--- back/src/routers/diaryRouter.js
+++ back/src/routers/diaryRouter.js
@@ -16,7 +16,7 @@ diaryRouter.post(
 
       //유저가 입력한 request body값
       const { plantId, imageURL, content, date } = req.body;
-      const status = req.body.status || null;
+      const status = req.body.status || "test";
 
       const newDiary = await diaryService.addDiary({
         userId,
@@ -42,17 +42,97 @@ diaryRouter.post(
 /**
  * 다이어리 상세 조회
  */
+diaryRouter.get(
+  "/diaries/:id", 
+  login_required, 
+  async (req, res, next) => {
+    try {
+      const diaryId = req.params.id;
+      const diary = await diaryService.getDiaryById(diaryId);
+      
+      const body = {
+        success: true,
+        diary: diary
+      }
+
+      res.status(200).json(body);
+    } catch (error){
+      next(error);
+    }
+  }
+);
 
 /**
  * 다이어리 목록 조회
  */
+diaryRouter.get(
+  "/diaries", 
+  login_required, 
+  async (req, res, next) => {
+    try{
+      const plantId = req.query.plantId || null;
+      const diaries = await diaryService.getDiaryByPlantId(plantId);
 
+      const body = {
+        success: true,
+        diaries: diaries,
+      }
+      res.status(200).json(body);
+    }catch(error){
+      next(error);
+    }
+  }
+);
 /**
  * 다이어리 수정
  */
 
+diaryRouter.put(
+  "/diaries/:id", 
+  login_required,
+  async (req, res, next) => {
+    try{
+      const diaryId = req.params.id;
+
+      const imageURL = req.body.imageURL ?? null;
+      const content = req.body.content ?? null;
+      const status = req.body.status ?? null;
+
+      const toUpdate = {
+        imageURL,
+        content,
+        status,
+      };
+
+      const updatedDiary = await diaryService.setDiary({ diaryId, toUpdate });
+
+      const body = {
+        success: true,
+        post: updatedDiary,
+      };
+
+      res.status(200).json(body);
+    } catch(error) {
+      next(error);
+    }
+  }
+);
+
 /**
  * 다이어리 삭제
  */
-
+diaryRouter.delete(
+  "/diaries/:id", 
+  login_required, 
+  async (req, res, next) => {
+    try {
+      const diaryId = req.params.id;
+      const isDeleted = await diaryService.deleteDiary(diaryId);
+  
+      res.status(200).json(isDeleted);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 export { diaryRouter };
