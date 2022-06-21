2ee3b209bfd9c2c379e130285510f6229c882857
brilence75 <brilence75@gmail.com>
2022-06-21 16:18:15 +0900

    Create : diary model 작성

---
+++ back/src/db/models/Diary.js
@@ -0,0 +1,35 @@
+import { DiaryModel } from "../schemas/diary";
+
+class Diary{
+  createDiary = async(Diary) => {
+    const newDiary = await DiaryModel.create(Diary);
+    return newDiary;
+  }
+  findDiaryById = async(diaryId) => {
+    const diary = await DiaryModel.findOne({_id: diaryId});
+    return diary;
+  }
+  findDiariesByPlantId = async(plantId) => {
+    const diaries = await DiaryModel.find(plantId);
+    return diaries;
+  }
+  
+  static update = async({diaryId, fieldToUpdate, newValue}) => {
+    const filter = {_id: diaryId};
+    const update = {[fieldToUpdate]: newValue};
+    const option = {returnOriginal: false};
+
+    const updatedPlant = await DiaryModel.findOneAndUpdate(
+      filter,
+      update,
+      option
+    );
+    return updatedPlant;
+  }
+
+  deleteDiaryById = async(diaryId) => {
+    await DiaryModel.deleteOne({_id : diaryId}, (result)=>{
+      return result.deletedCount === 1;
+    });
+  }
+}
