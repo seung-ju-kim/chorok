38edb965e06dcc80f748e986a45b8a605df54265
sj_kim <ksj961223@gmail.com>
2022-06-23 19:51:45 +0900

    [Add] mygarden, care, diary 스켈레톤 UI 적용

--- front/src/components/mygarden/MyGardenCard.js
+++ front/src/components/mygarden/MyGardenCard.js
@@ -1,36 +1,32 @@
 import React from "react";
-import { Card, CardMedia, Typography, Grid } from "@mui/material";
+import { Card, CardMedia, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
-const MyGardenCard = ({ myplant }) => {
+const MyGardenCard = ({ myplant, isLoading }) => {
   const navigate = useNavigate();
 
   return (
-    <>
-      <Grid item xs={6}>
-        <Card
-          sx={{
-            boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-            cursor: "pointer",
-            maxWidth: "250px",
-            mx: "auto",
-          }}
-        >
-          <CardMedia
-            component="img"
-            image={myplant.imageURL}
-            sx={{ objectFit: "fill", height: "150px" }}
-            onClick={() => {
-              navigate(`/mygarden/${myplant._id}`);
-            }}
-          />
-
-          <Typography variant="h6" textAlign="center">
-            {myplant.nickname}
-          </Typography>
-        </Card>
-      </Grid>
-    </>
+    <Card
+      sx={{
+        boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+        cursor: "pointer",
+        maxWidth: "250px",
+        height: "180px",
+        mx: "auto",
+      }}
+    >
+      <CardMedia
+        component="img"
+        image={myplant.imageURL}
+        sx={{ objectFit: "fill", height: "150px" }}
+        onClick={() => {
+          navigate(`/mygarden/${myplant._id}`);
+        }}
+      />
+      <Typography variant="h6" textAlign="center">
+        {myplant.nickname}
+      </Typography>
+    </Card>
   );
 };
 
--- front/src/components/schedule/MyScheduleList.js
+++ front/src/components/schedule/MyScheduleList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
+import MyScheduleCard from "./MyScheduleCard";
 
 const MyScheduleList = () => {
   return (
@@ -11,6 +12,7 @@ const MyScheduleList = () => {
       >
         나의 스케줄
       </Typography>
+      <MyScheduleCard />
     </Box>
   );
 };
