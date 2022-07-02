81ffa52fef771d6b9f441fbb694102d066c42a2c
sj_kim <ksj961223@gmail.com>
2022-07-02 19:23:46 +0900

    [Fix] 나의 정원 추가하기 버튼 화면 작을 경우 사라지는 오류 수정

--- front/src/components/mygarden/MyGardenCardList.js
+++ front/src/components/mygarden/MyGardenCardList.js
@@ -75,7 +75,7 @@ const MyGardenCardList = ({ initMyPlants = [] }) => {
                 sx={{
                   boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
                   color: "#64a68a",
-                  p: 10,
+                  p: 7,
                   position: "absolute",
                   width: "100%",
                   height: "100%",
